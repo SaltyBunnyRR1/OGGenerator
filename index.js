117f9cd386817fd040df44cdb7793e6fa9d4832c
SaltyBunnyRR1 <66356621+SaltyBunnyRR1@users.noreply.github.com>
2020-06-29 06:38:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const fetch = require("node-fetch")
-// # Created By SaltyBunnyRR1#5600 On Discord.
-console.log('Made By SaltyBunnyRR1#5600 On Discord.')
-
-setInterval(async() => {
-	fetch("https://random-word-api.herokuapp.com/word?number=1").then(res => res.json())
-    .then(name => {
-fetch("https://api.mojang.com/users/profiles/minecraft/" + name).then(res => res.json()).then(json => {
-if(json){ return }
-}).catch(err => { 
-	if(name[0].length > 16 || name[0].length < 3) return;
-	console.log("[!]")
-console.log(name[0] + " - " + name[0].length)
-console.log('[!]')
-})
-})
-	}, 1000)
+// # Made By SaltyBunnyRR1#5600 On Discord 
+console.log('Made By SaltyBunnyRR1#5600 On Discord ')
+setInterval(() => {
+fetch("https://random-word-api.herokuapp.com/word?number=1")
+.then(res => res.json())
+.then(name => {
+fetch("https://api.mojang.com/users/profiles/minecraft/" + name[0])
+.then(res => res.json())
+.then(json => {
+	if(json) return;
+}).catch(() => {
+	if(name[0].length > 2 && name[0].length < 17){
+console.log(`${name[0]} - ${name[0].length}`)
+}
+})//catch json
+})//name
+}, 1000)//SetInterval
