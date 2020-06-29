b4213db93c4acedff0226e87cefcd4f2802b0423
SaltyBunnyRR1 <66356621+SaltyBunnyRR1@users.noreply.github.com>
2020-06-29 04:23:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ console.log('Made By SaltyBunnyRR1#5600 On Discord.')
 setInterval(async() => {
 	fetch("https://random-word-api.herokuapp.com/word?number=1").then(res => res.json())
     .then(name => {
-console.log(`Checking ${name[0]}...`)
 fetch("https://api.mojang.com/users/profiles/minecraft/" + name).then(res => res.json()).then(json => {
 if(json){ return }
 }).catch(err => { 
 	if(name[0].length > 16 || name[0].length < 3) return;
+	console.log("[!]")
 console.log(name[0] + " - " + name[0].length)
-
+console.log('[!]')
 })
 })
 	}, 1000)
