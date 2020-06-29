1fba77b12565514ff67d0af199e3758bf4996086
SaltyBunnyRR1 <66356621+SaltyBunnyRR1@users.noreply.github.com>
2020-06-29 03:39:00 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch")
+// # Created By SaltyBunnyRR1#5600 On Discord.
+console.log('Made By SaltyBunnyRR1#5600 On Discord.')
+
+setInterval(async() => {
+	fetch("https://random-word-api.herokuapp.com/word?number=1").then(res => res.json())
+    .then(name => {
+console.log(`Checking ${name[0]}...`)
+fetch("https://api.mojang.com/users/profiles/minecraft/" + name).then(res => res.json()).then(json => {
+if(json){ return }
+}).catch(err => { 
+	if(name[0].length > 16 || name[0].length < 3) return;
+console.log(name[0] + " - " + name[0].length)
+
+})
+})
+	}, 1000)
